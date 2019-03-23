8fdc48a7983b8da406c6ae17119cc0f2813e9165
RishYang <yangrshin@gmail.com>
2019-03-24 02:49:25 +0800

    All Ch7

---
+++ Ch7/practical7_3.pl
@@ -0,0 +1,9 @@
+
+prop --> [p].
+prop --> [q].
+prop --> [r].
+prop --> [not], prop.
+prop --> ['('], prop, ['and'], prop, [')'].
+prop --> ['('], prop, ['or'], prop, [')'].
+prop --> ['('], prop, ['implies'], prop, [')'].
+
